d7842554aeb6c29a1f72020648010bc40c00371c
Raul Alvarez <raulalvarezrom3@gmail.com>
2024-06-18 21:17:56 -0600

    actualización de la app.py con la casilla de verificación

--- app.py
+++ app.py
@@ -16,3 +16,24 @@ if hist_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
+        
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+build_boxplot = st.checkbox('Construir un boxplot')
+
+if build_boxplot: # si la casilla de verificación está seleccionada
+    st.write('Construir un boxplot para la columna odómetro')
+    
+    fig = px.box(car_data, x="condition", y="price", color="fuel")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
